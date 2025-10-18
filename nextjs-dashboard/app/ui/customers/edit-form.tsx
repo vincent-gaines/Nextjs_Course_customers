f70aaa905cfb90fc9f94c0819bc5c7e2b8566d5a
vincent-gaines <vincentanggaines@gmail.com>
2025-10-18 13:37:45 -0400

    Refactor EditCustomerForm to update customer fields, including email and status, and improve form structure for better data handling and UI consistency.

--- nextjs-dashboard/app/ui/customers/edit-form.tsx
+++ nextjs-dashboard/app/ui/customers/edit-form.tsx
@@ -9,17 +9,18 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateCustomer, State } from '@/app/lib/actions';
+import { updateCustomer, StateCustomer } from '@/app/lib/actions';
 import { useActionState } from 'react';
+import { customers } from '@/app/lib/placeholder-data';
  
 export default function EditCustomerForm({
   Customer,
-  // customers,
+  customers,
 }: {
   Customer: CustomerForm;
- // customers: CustomerField[];
+  customers: CustomerField[];
 }) {
-  const initialState: State = { message: null, errors: {} };
+  const initialState: StateCustomer = { message: null, errors: {} };
   const updateCustomerWithId = updateCustomer.bind(null, Customer.id);
   const [state, formAction] = useActionState(updateCustomerWithId, initialState);
   return (
@@ -35,7 +36,7 @@ export default function EditCustomerForm({
               id="customer"
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={Customer.customer_id}
+              defaultValue={Customer.id}
             >
               <option value="" disabled>
                 Select a customer
@@ -49,27 +50,25 @@ export default function EditCustomerForm({
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
+        
 
-        {/* Customer Amount */}
+        {/* Customer Email */}
         <div className="mb-4">
-          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Choose an amount
+          <label htmlFor="email" className="mb-2 block text-sm font-medium">
+            Customer Email
           </label>
-          <div className="relative mt-2 rounded-md">
-            <div className="relative">
-              <input
-                id="amount"
-                name="amount"
-                type="number"
-                step="0.01"
-                defaultValue={Customer.amount}
-                placeholder="Enter USD amount"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
+          <div className="relative">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              defaultValue={Customer.email}
+              placeholder="Enter customer email"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+            />
           </div>
         </div>
+      </div>
 
         {/* Customer Status */}
         <fieldset>
@@ -96,24 +95,61 @@ export default function EditCustomerForm({
               </div>
               <div className="flex items-center">
                 <input
-                  id="paid"
+                  id="active"
                   name="status"
                   type="radio"
-                  value="paid"
-                  defaultChecked={Customer.status === 'paid'}
+                  value="active"
+                  defaultChecked={Customer.status === 'active'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
-                  htmlFor="paid"
+                  htmlFor="active"
                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
                 >
-                  Paid <CheckIcon className="h-4 w-4" />
+                  Active <CheckIcon className="h-4 w-4" />
                 </label>
               </div>
             </div>
           </div>
         </fieldset>
-      </div>
+      
+
+      {/* Customer image_url */}
+      <div className="mb-4">
+        <label htmlFor="image_url" className="mb-2 block text-sm font-medium">
+            Customer Image URL
+          </label>
+          <div className="relative">
+            <input
+              id="image_url"
+              name="image_url"
+              type="text"
+              defaultValue={Customer.image_url}
+              placeholder="Enter customer image URL"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+            />
+          </div>
+        </div>
+ 
+  
+        {/* Customer date */}
+        <div className="mb-4">
+          <label htmlFor="date" className="mb-2 block text-sm font-medium">
+            Customer Date
+          </label>
+          <div className="relative">
+            
+            <input
+              id="date"
+              name="date"
+              type="text"
+              defaultValue={Customer.date}
+              placeholder="Enter customer date"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+            />
+          </div>
+        </div>
+
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/Customers"
